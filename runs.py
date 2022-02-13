6f3e8027a6be691a3a5b9a5339a242dd627946b8
sushantsur23 <sushant.sur23@gmail.com>
2022-02-13 09:26:21 +0530

    final commit

---
+++ runs.py
@@ -0,0 +1,8 @@
+import os
+p1s = range(1,5)
+p2s = range(1,5)
+
+for p1 in p1s:
+    for p2 in p2s:
+        print(f"logging experiment for p1: {p1}, p2: {p2}")
+        os.system(f"python demo.py -p1 {p1} -p2 {p2}")
